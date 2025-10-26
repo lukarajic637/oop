2f8f9f2e0e256df41e39a500d07016e7b4126898
lukarajic637 <lukarajic52@gmail.com>
2025-10-26 22:57:09 +0100

    Add files via upload

---
+++ zad2vj2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Vector {
+    int* elementi;
+    int logicka;
+    int fizicka;
+};
+
+Vector vector_new(int init) { // init = 2
+    Vector v;
+    v.elementi = new int[init]; // [  ]
+    v.logicka = 0;
+    v.fizicka = init; // 2
+    return v;
+}
+
+void vector_delete(Vector& v) {
+    delete[] v.elementi;
+    v.elementi = nullptr;
+    v.logicka = 0;
+    v.fizicka = 0;
+}
+
+void vector_push_back(Vector& v, int vrijednost) {
+    if (v.logicka >= v.fizicka) { // 2>=2
+        int novaFizicka = v.fizicka * 2; // novaFizicka = 4
+        int* noviNiz = new int[novaFizicka];  // [    ]
+        for (int i = 0; i < v.logicka; i++) { // 0,1,2
+            noviNiz[i] = v.elementi[i]; // noviniz[2] = v.elementi[2](15)
+            // [5, 10 , 15]
+        }
+        delete[] v.elementi; // v.elementi = []
+        v.elementi = noviNiz; // v.elementi = [5 ,10 ,15,  ]
+        v.fizicka = novaFizicka; // v.fizicka = 4
+    }
+    v.elementi[v.logicka] = vrijednost;  // v.elementi[2] = 15
+    v.logicka++; // 3
+}
+
+void vector_pop_back(Vector& v) {
+    if (v.logicka > 0) { // 3>0
+        v.logicka--;  // v.logicka = 2
+    }
+}
+
+int vector_front(Vector& v) {
+    return v.elementi[0]; // 5 
+}
+
+int vector_back(Vector& v) {
+    return v.elementi[v.logicka - 1]; // 15
+}
+
+int vector_size(Vector& v) { 
+    return v.logicka; 
+}
+
+int main() {
+    Vector v = vector_new(2);
+
+    vector_push_back(v, 5);
+    vector_push_back(v, 10);
+    vector_push_back(v, 15);
+
+    cout << "Prvi element: " << vector_front(v) << endl;
+    cout << "Zadnji element: " << vector_back(v) << endl;
+    cout << "Velicina: " << vector_size(v) << endl;
+
+    vector_pop_back(v);
+    cout << "Nakon pop_back, velicina: " << vector_size(v) << endl;
+
+    vector_delete(v);
+    return 0;
+}
---
+++ zad3vj2.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+struct Matrica {
+    int m;          // broj redaka
+    int n;          // broj stupaca
+    double** elementi; 
+};
+
+Matrica novaMatrica(int m, int n) { // 3 2
+    Matrica mat;
+    mat.m = m;
+    mat.n = n;
+    mat.elementi = new double* [m]; //[3]== [[],[],[]]
+    for (int i = 0; i < m; i++)
+        mat.elementi[i] = new double[n];//mat.elementi[2] = new double[2]
+    return mat; // [[  ],[  ],[  ]]
+}
+
+void brisiMatricu(Matrica& mat) {
+    for (int i = 0; i < mat.m; i++)
+        delete[] mat.elementi[i];  
+    delete[] mat.elementi;
+    mat.elementi = nullptr;
+    mat.m = 0;
+    mat.n = 0;
+}
+
+void unosMatrice(Matrica& mat) {
+    for (int i = 0; i < mat.m; i++)
+        for (int j = 0; j < mat.n; j++) { /
+            cout << "mat[" << i << "][" << j << "] = ";
+            cin >> mat.elementi[i][j]; // [0][0], [0][1], [1][0], [1][1], [2][0]
+        }
+}
+
+void generirajMatricu(Matrica& mat, double a, double b) {
+    srand(time(nullptr));
+    for (int i = 0; i < mat.m; i++)
+        for (int j = 0; j < mat.n; j++)
+            mat.elementi[i][j] = a + (double)rand() / RAND_MAX * (b - a);
+}
+
+void ispisMatrice(const Matrica& mat) {
+    for (int i = 0; i < mat.m; i++) {
+        for (int j = 0; j < mat.n; j++)
+            cout << setw(10) << fixed << setprecision(4) << mat.elementi[i][j];
+        cout << endl;
+    }
+}
+
+Matrica zbrojMatrica(const Matrica& A, const Matrica& B) {
+    Matrica C = novaMatrica(A.m, A.n);
+    for (int i = 0; i < A.m; i++)
+        for (int j = 0; j < A.n; j++)
+            C.elementi[i][j] = A.elementi[i][j] + B.elementi[i][j];
+    return C;
+}
+
+Matrica razlikaMatrica(const Matrica& A, const Matrica& B) {
+    Matrica C = novaMatrica(A.m, A.n);
+    for (int i = 0; i < A.m; i++)
+        for (int j = 0; j < A.n; j++)
+            C.elementi[i][j] = A.elementi[i][j] - B.elementi[i][j];
+    return C;
+}
+
+Matrica transponiraj(const Matrica& A) {
+    Matrica T = novaMatrica(A.n, A.m);
+    for (int i = 0; i < A.m; i++)
+        for (int j = 0; j < A.n; j++)
+            T.elementi[j][i] = A.elementi[i][j];
+    return T;
+}
+
+int main() {
+    int m, n;
+    cout << "Unesi broj redaka i stupaca: ";
+    cin >> m >> n;
+
+    Matrica A = novaMatrica(m, n);
+    Matrica B = novaMatrica(m, n);
+
+    cout << "Unesi elemente prve matrice:\n";
+    unosMatrice(A);
+
+    cout << "Generiram drugu matricu s nasumičnim brojevima...\n";
+    generirajMatricu(B, 0, 5);
+
+    cout << "\nMatrica A:\n";
+    ispisMatrice(A);
+
+    cout << "\nMatrica B:\n";
+    ispisMatrice(B);
+
+    Matrica Z = zbrojMatrica(A, B);
+    cout << "\nZbroj A + B:\n";
+    ispisMatrice(Z);
+
+    Matrica R = razlikaMatrica(A, B);
+    cout << "\nRazlika A - B:\n";
+    ispisMatrice(R);
+
+    Matrica T = transponiraj(A);
+    cout << "\nTransponirana matrica A:\n";
+    ispisMatrice(T);
+
+    brisiMatricu(A);
+    brisiMatricu(B);
+    brisiMatricu(Z);
+    brisiMatricu(R);
+    brisiMatricu(T);
+
+    return 0;
+}
