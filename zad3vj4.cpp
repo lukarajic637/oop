dfe2dd1bf9de279223eedfcf0db78237f0f26bea
lukarajic637 <lukarajic52@gmail.com>
2025-11-17 00:21:36 +0100

    Add files via upload

---
+++ zad3vj4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+void reverse_strings(std::vector<std::string>& words) {
+    for (auto& w : words) {
+        std::reverse(w.begin(), w.end());
+    }
+}
+
+
+int main() {
+    std::vector<std::string> v = { "hello", "world", "c++" };
+
+    std::cout << "Prije: ";
+    for (auto& s : v) std::cout << s << " ";
+
+    reverse_strings(v);
+
+    std::cout << "\nPoslije: ";
+    for (auto& s : v) std::cout << s << " ";
+
+    return 0;
+}
---
+++ zad4vj4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+
+std::string word_to_pig_latin(const std::string& word) {
+    if (word.empty()) return word;
+
+    std::string vowels = "aeiouAEIOU";
+
+    if (vowels.find(word[0]) != std::string::npos) {
+        return word + "hay";
+    }
+    return word.substr(1) + word[0] + "ay";
+}
+
+int main() {
+    std::string w1 = "apple";
+    std::string w2 = "hello";
+    std::string w3 = "string";
+
+    std::cout << w1 << " -> " << word_to_pig_latin(w1) << "\n";
+    std::cout << w2 << " -> " << word_to_pig_latin(w2) << "\n";
+    std::cout << w3 << " -> " << word_to_pig_latin(w3) << "\n";
+
+    return 0;
+}
